adb443d1e5a089c1f54909a73b4e18e8d9a5ad49
Victor Vicente Palacios <victorvicpal@gmail.com>
2016-11-12 17:52:54 +0100

    Taller geoestadística

--- TallerGeo.R
+++ TallerGeo.R
@@ -19,19 +19,15 @@ plot(density(meuse$zinc),main='Densidad Zn',col="blue")
 plot(density(meuse$cadmium),main='Densidad Cd',col="orange")
 plot(density(meuse$copper),main='Densidad Cu',col="red")
 plot(density(meuse$lead),main='Densidad Pb',col="grey")
+#Veamos qué sucede aplicando una transformación logarítmica
+plot(density(log(meuse$zinc)),main='Densidad Zn',col="blue")
+plot(density(log(meuse$cadmium)),main='Densidad Cd',col="orange")
+plot(density(log(meuse$copper)),main='Densidad Cu',col="red")
+plot(density(log(meuse$lead)),main='Densidad Pb',col="grey")
 
 #Nuestro principal objetivo es ver el comportamiento de las variables Zinc, Cadmio, Cobre y Plomo.
 AnData <- meuse[,c(1:6,8)] 
 cor(AnData[,3:6])	#Matriz de correlacion
-
-#Estandarizamos los datos
-medias	<-	apply(AnData[,3:6],2,mean)
-desv	<-	apply(AnData[,3:6],2,sd)
-
-for (i in 1:4)
-{AnData[,i+2] <- (AnData[,i+2]-medias[i])/desv[i]}
-
-boxplot(AnData$cadmium,AnData$copper,AnData$lead,AnData$zinc,names=c('Cd','Cu','Pb','Zn'))
 
 #Transformación espacial de los datos
 coordinates(AnData) <- ~x+y
@@ -64,7 +60,8 @@ m3	<-	plotGoogleMaps(meuse,filename='MiMapa3.htm',iconMarker='http://maps.google
 
 ##KRIGING##
 #Variograma#
-vg.zn <- variogram(zinc~1, AnData)
+vg.zn <- variogram(log(zinc)~1, AnData)
+dev.off() #Borramos los gráficos anteriores
 plot(vg.zn)
 vg.zn
 
@@ -82,7 +79,7 @@ coordinates(meuse.grid) <- ~x+y
 plot(meuse.grid)
 
 ##Ordinary Kriging##
-zn.krig <- krige(zinc~1, AnData, meuse.grid, model = vg.zn.fit)
+zn.krig <- krige(log(zinc)~1, AnData, meuse.grid, model = vg.zn.fit)
 names(zn.krig)
 p1 <- spplot(zn.krig["var1.pred"])
 p2 <- spplot(zn.krig["var1.var"])
@@ -90,7 +87,7 @@ print(p1, split=c(1,1,2,1), more=TRUE)
 print(p2, split=c(2,1,2,1), more=TRUE)
 
 #Crossvalidation#
-cv.ok <- krige.cv(zinc~1, AnData, model=vg.zn.fit)
+cv.ok <- krige.cv(log(zinc)~1, AnData, model=vg.zn.fit)
 summary(cv.ok)
 resid.ok <- as.data.frame(cv.ok)$residual
 sqrt(mean(resid.ok^2))
@@ -98,12 +95,12 @@ mean(resid.ok)
 mean(resid.ok^2/as.data.frame(cv.ok)$var1.var)
 
 ##Universal Kriging##
-vg.zn.dist <- variogram(zinc~sqrt(dist), AnData)
+vg.zn.dist <- variogram(log(zinc)~sqrt(dist), AnData)
 vg.zn.dist.fit <- fit.variogram(vg.zn.dist, model = vgm(1, "Sph", 900, 1))
-zn.krig.dist <- krige(zinc~sqrt(dist), AnData, meuse.grid, model = vg.zn.dist.fit)
+zn.krig.dist <- krige(log(zinc)~sqrt(dist), AnData, meuse.grid, model = vg.zn.dist.fit)
 
 #Crossvalidation#
-cv.uk <- krige.cv(zinc~sqrt(dist), AnData, model=vg.zn.dist.fit)
+cv.uk <- krige.cv(log(zinc)~sqrt(dist), AnData, model=vg.zn.dist.fit)
 summary(cv.uk)
 resid.uk <- as.data.frame(cv.uk)$residual
 sqrt(mean(resid.uk^2))
@@ -112,17 +109,17 @@ mean(resid.uk^2/as.data.frame(cv.uk)$var1.var)
 
 ##COKRIGING##
 #Ordinary Cokriging#
-gock <- gstat(NULL, "zn", zinc~1, AnData)
-gock <- gstat(gock, "cd", cadmium~1, AnData)
-gock <- gstat(gock, "cu", copper~1, AnData)
-gock <- gstat(gock, "pb", lead~1, AnData)
+gock <- gstat(NULL, "zn", log(zinc)~1, AnData)
+gock <- gstat(gock, "cd", log(cadmium)~1, AnData)
+gock <- gstat(gock, "cu", log(copper)~1, AnData)
+gock <- gstat(gock, "pb", log(lead)~1, AnData)
 
 #Se puede realizar un Cokriging Universal de manera parecida a cómo se realizó en el caso del kriging#
 #Universal Cokriging#
-guck <- gstat(NULL, "zn", zinc~sqrt(dist), AnData)
-guck <- gstat(guck, "cd", cadmium~sqrt(dist), AnData)
-guck <- gstat(guck, "cu", copper~sqrt(dist), AnData)
-guck <- gstat(guck, "pb", lead~sqrt(dist), AnData)
+guck <- gstat(NULL, "zn", log(zinc)~sqrt(dist), AnData)
+guck <- gstat(guck, "cd", log(cadmium)~sqrt(dist), AnData)
+guck <- gstat(guck, "cu", log(copper)~sqrt(dist), AnData)
+guck <- gstat(guck, "pb", log(lead)~sqrt(dist), AnData)
 
 #Variogramas Cruzados#
 #Ordinary Cokriging#
@@ -150,46 +147,45 @@ cko <- predict(gock.fit, meuse.grid)
 #Universal Cokriging#
 cku <- predict(guck.fit, meuse.grid)
 
-#Cross-validation
-#Ordinary Cokriging#
-crva.cko <- gstat.cv(gock.fit,all.residuals = T)
-summary(crva.cko)
-plot(density(crva.cko$zn))
-
-#Universal Cokriging#
-crva.cku <- gstat.cv(guck.fit,all.residuals = T)
-summary(crva.cku)
-plot(density(cv.cku$cu))
-
 #Visualizamos los resultados
-pl1 <- spplot(cku["zn.pred"], main="Zinc")
-pl2 <- spplot(cku["cu.pred"], main="Cobre")
-pl3 <- spplot(cku["cd.pred"], main="Cadmio")
-pl4 <- spplot(cku["pb.pred"], main="Plomo")
+pl1 <- spplot(cku["zn.pred"], main="Zinc Universal")
+pl1b <- spplot(cko["zn.pred"], main="Zinc Ordinary")
+pl2 <- spplot(cku["cu.pred"], main="Cobre Universal")
+pl2b <- spplot(cko["cu.pred"], main="Cobre Ordinary")
+pl3 <- spplot(cku["cd.pred"], main="Cadmio Universal")
+pl3b <- spplot(cko["cd.pred"], main="Cadmio Ordinary")
+pl4 <- spplot(cku["pb.pred"], main="Plomo Universal")
+pl4b <- spplot(cku["pb.pred"], main="Plomo Ordinary")
 
-print(pl1, split = c(1,1,2,2), more=TRUE)
-print(pl2, split = c(1,2,2,2), more=TRUE)
-print(pl3, split = c(2,1,2,2), more=TRUE)
-print(pl4, split = c(2,2,2,2))
+print(pl1, split = c(1,1,4,2), more=TRUE)
+print(pl1b, split = c(2,1,4,2), more=TRUE)
+print(pl2, split = c(3,1,4,2), more=TRUE)
+print(pl2b, split = c(4,1,4,2), more=TRUE)
+print(pl3, split = c(1,2,4,2), more=TRUE)
+print(pl3b, split = c(2,2,4,2), more=TRUE)
+print(pl4, split = c(3,2,4,2), more=TRUE)
+print(pl4b, split = c(4,2,4,2))
 
 #Crossvalidation#
 #Ordinary#
 cv.ck.ok <- gstat.cv(gock.fit)
 summary(cv.ck.ok)
-resid.ok <- as.data.frame(cv.ck.ok)$residual
-sqrt(mean(resid.ok^2))
-mean(resid.ok)
-mean(resid.ok^2/as.data.frame(cv.ck.ok)$au.var)
+resid.cko <- as.data.frame(cv.ck.ok)$residual
+sqrt(mean(resid.cko^2))
+mean(resid.cko)
+mean(resid.cko^2/as.data.frame(cv.ck.ok)$zn.var)
 ##Si quieres calcular todos los residuales añade la opción "all.residuals = TRUE" en gstat.cv#
-cv.ck.ok <- gstat.cv(gock.fit,all.residuals=TRUE)
+cv.ck.ok.all <- gstat.cv(gock.fit,all.residuals=TRUE)
+summary(cv.ck.ok.all)
 
 
 #Universal#
 cv.ck.uk <- gstat.cv(guck.fit)
 summary(cv.ck.uk)
-resid.uk <- as.data.frame(cv.ck.uk)$residual
-sqrt(mean(resid.uk^2))
-mean(resid.uk)
-mean(resid.uk^2/as.data.frame(cv.ck.uk)$au.var)
+resid.cku <- as.data.frame(cv.ck.uk)$residual
+sqrt(mean(resid.cku^2))
+mean(resid.cku)
+mean(resid.cku^2/as.data.frame(cv.ck.uk)$zn.var)
 #Todos los residuales#
-cv.ck.uk <- gstat.cv(gock.fit,all.residuals=TRUE)
+cv.ck.uk.all <- gstat.cv(gock.fit,all.residuals=TRUE)
+summary(cv.ck.uk.all)
